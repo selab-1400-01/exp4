d6a463efde94321f2acb15a3f6b2b1e5211d815f
momt99 <mohammad.omt99@yahoo.com>
2021-11-09 17:28:07 +0330

    Add rainy garden factory default implementation

---
+++ src/main/java/selab/exp4/garden/factory/RainyGardenFactory.java
@@ -0,0 +1,22 @@
+package selab.exp4.garden.factory;
+
+import selab.exp4.garden.plant.bush.Bush;
+import selab.exp4.garden.plant.tree.decorative.DecorativeTree;
+import selab.exp4.garden.plant.tree.fruit.FruitTree;
+
+public class RainyGardenFactory extends GardenFactory {
+    @Override
+    public DecorativeTree createDecorativeTree() {
+        return null;
+    }
+
+    @Override
+    public FruitTree createFruitTree() {
+        return null;
+    }
+
+    @Override
+    public Bush createBush() {
+        return null;
+    }
+}
