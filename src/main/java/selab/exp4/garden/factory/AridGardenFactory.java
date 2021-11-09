2c90ed7bdd6c42a1205e691041e6923ebbe49971
momt99 <mohammad.omt99@yahoo.com>
2021-11-09 17:22:04 +0330

    Complete arid garden factory

--- src/main/java/selab/exp4/garden/factory/AridGardenFactory.java
+++ src/main/java/selab/exp4/garden/factory/AridGardenFactory.java
@@ -1,22 +1,25 @@
 package selab.exp4.garden.factory;
 
 import selab.exp4.garden.plant.bush.Bush;
+import selab.exp4.garden.plant.bush.ThornyBush;
 import selab.exp4.garden.plant.tree.decorative.DecorativeTree;
+import selab.exp4.garden.plant.tree.decorative.PalmTree;
 import selab.exp4.garden.plant.tree.fruit.FruitTree;
+import selab.exp4.garden.plant.tree.fruit.PistachioTree;
 
 public class AridGardenFactory extends GardenFactory {
     @Override
     public DecorativeTree createDecorativeTree() {
-        return null;
+        return new PalmTree();
     }
 
     @Override
     public FruitTree createFruitTree() {
-        return null;
+        return new PistachioTree();
     }
 
     @Override
     public Bush createBush() {
-        return null;
+        return new ThornyBush();
     }
 }
