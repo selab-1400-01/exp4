7f685247a7d842fdeae92650eb15d38db8394532
momt99 <mohammad.omt99@yahoo.com>
2021-11-09 18:23:00 +0330

    Add some rainy plants

---
+++ src/main/java/selab/exp4/garden/plant/tree/decorative/CedarTree.java
@@ -0,0 +1,8 @@
+package selab.exp4.garden.plant.tree.decorative;
+
+public class CedarTree extends DecorativeTree {
+    @Override
+    public int getWaterConsumption() {
+        return 5000;
+    }
+}
---
+++ src/main/java/selab/exp4/garden/plant/tree/fruit/MapleTree.java
@@ -0,0 +1,8 @@
+package selab.exp4.garden.plant.tree.fruit;
+
+public class MapleTree extends FruitTree {
+    @Override
+    public int getWaterConsumption() {
+        return 10000;
+    }
+}
